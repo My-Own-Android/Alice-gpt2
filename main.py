ed5444e40da132ead95ec6550b9d226e4e80d21e
Арен Елчинян <slizen.can@gmail.com>
2023-04-07 18:36:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import gpt_2_simple as gpt2
-import os
+import os, time
 
 model_name = "124M"
 if not os.path.isdir(os.path.join("models", model_name)):
@@ -15,12 +15,23 @@ file_name = "alice_and_aren.txt"
 	#with open(file_name, 'w') as f:
 	#	 f.write(data.text)
 
-
+_start = time.time()
 sess = gpt2.start_tf_sess()
+print(f"gpt2.start_tf_sess заняло: {time.time() - _start}")
+
+_start = time.time()
 gpt2.finetune(sess,
               file_name,
               model_name=model_name,
               steps=10)   # steps is max number of training steps
+print(f"gpt2.finetune заняло: {time.time() - _start}")
 
+print("export 1")
+_start = time.time()
 gpt2.generate_to_file(sess, destination_path='export/gpt_2_gen_texts.txt')
+print(f"Экспорт 1 занял: {time.time() - _start}")
+
+print("export 2")
+_start = time.time()
 gpt2.generate(sess)
+print(f"Экспорт 2 занял: {time.time() - _start}")
